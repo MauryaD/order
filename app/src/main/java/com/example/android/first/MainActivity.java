da32facbaf03bdf03fcbf9a8c3e2b0f79c393a5e
Deepak Maurya <deepakmaurya2198@gmail.com>
2018-11-12 23:34:18 +0530

    initial commit again

--- app/src/main/java/com/example/android/first/MainActivity.java
+++ app/src/main/java/com/example/android/first/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.android.first;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.text.NumberFormat;
 
 
+
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,4 +46,16 @@ public class MainActivity extends AppCompatActivity {
         TextView price=(TextView) findViewById(R.id.textView2);
         price.setText("Total Price= "+NumberFormat.getCurrencyInstance().format(Num)+ "\n Thank You!!");
     }
+    public void next(View view){
+        Button next= (Button)findViewById(R.id.next);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent newactivity= new Intent(MainActivity.this, Activity2.class);
+                startActivity(newactivity) ;
+            }
+        });
+
+    }
+
 }
