3a8afae905a6a0d7b4771985666af1f52256a70d
Deepak Maurya <deepakmaurya2198@gmail.com>
2018-11-13 18:55:47 +0530

    Added checkboxes!!

--- app/src/main/java/com/example/android/first/MainActivity.java
+++ app/src/main/java/com/example/android/first/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.first;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,14 +8,16 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.NumberFormat;
 import java.util.DoubleSummaryStatistics;
 
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText et;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +27,9 @@ public class MainActivity extends AppCompatActivity {
     public void Order(View view){
         //int quantity=4;
         //System.out.println("Total Price is: ");
+        et = findViewById(R.id.Edit);
+        String name = et.getText().toString();
+
         CheckBox CB1 = findViewById(R.id.CB1);
         boolean cb = CB1.isChecked();
         Log.v("MainActivity", "Has whipped cream: " + cb);
@@ -31,16 +37,26 @@ public class MainActivity extends AppCompatActivity {
         boolean cb2 = CB2.isChecked();
         Log.v("MainActivity", "Has Chocolate: " + cb2);
 
-
         displayprice(quan * 8, cb, cb2);
     }
 
     public void increament(View view){
         quan++;
+        if (quan > 5) {
+            Context context = getApplicationContext();
+            Toast.makeText(context, "Please Enter the quantity greater than 0",
+                    Toast.LENGTH_SHORT).show();
+        }
+
         display(quan);
     }
     public void decre(View view){
         quan--;
+        if (quan < 0) {
+            Context context = getApplicationContext();
+            Toast.makeText(context, "Please Enter the quantity greater than 0",
+                    Toast.LENGTH_SHORT).show();
+        }
         display(quan);
 
     }
