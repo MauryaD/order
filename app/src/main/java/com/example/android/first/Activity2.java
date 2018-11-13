e4257e895bfe7f219921f9e1420359b4528c624a
Deepak Maurya <deepakmaurya2198@gmail.com>
2018-11-13 17:37:41 +0530

    Added checkboxes!!

--- app/src/main/java/com/example/android/first/Activity2.java
+++ app/src/main/java/com/example/android/first/Activity2.java
@@ -2,12 +2,19 @@ package com.example.android.first;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Activity2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_2);
+        TextView tv = new TextView(this);
+        //tv.setText("hurrayyyy!!");
+        tv.getTextColors();
+        setContentView(tv);
+
+
+        //setContentView(R.layout.activity_2);
     }
 }
--- app/src/main/java/com/example/android/first/MainActivity.java
+++ app/src/main/java/com/example/android/first/MainActivity.java
@@ -3,17 +3,17 @@ package com.example.android.first;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 import java.text.NumberFormat;
-
+import java.util.DoubleSummaryStatistics;
 
 
 public class MainActivity extends AppCompatActivity {
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +24,15 @@ public class MainActivity extends AppCompatActivity {
     public void Order(View view){
         //int quantity=4;
         //System.out.println("Total Price is: ");
-        display(quan);
-        displayprice(quan*8);
+        CheckBox CB1 = findViewById(R.id.CB1);
+        boolean cb = CB1.isChecked();
+        Log.v("MainActivity", "Has whipped cream: " + cb);
+        CheckBox CB2 = findViewById(R.id.CB2);
+        boolean cb2 = CB2.isChecked();
+        Log.v("MainActivity", "Has Chocolate: " + cb2);
+
+
+        displayprice(quan * 8, cb, cb2);
     }
 
     public void increament(View view){
@@ -42,9 +49,18 @@ public class MainActivity extends AppCompatActivity {
         TextView quantityView=(TextView) findViewById(R.id.textView);
         quantityView.setText("" + n);
     }
-    private void displayprice(int Num){
+
+    private void displayprice(int Num, boolean h, boolean g) {
         TextView price=(TextView) findViewById(R.id.textView2);
-        price.setText("Total Price= "+NumberFormat.getCurrencyInstance().format(Num)+ "\n Thank You!!");
+
+        String summary;
+        summary = "Total Price= " + NumberFormat.getCurrencyInstance().format(Num);
+        summary += "\nAdd whipped cream? " + h;
+        summary += "\nAdd Chocolate? " + g;
+        summary += "\nQuantity= " + quan;
+        summary += "\n Thank You!!";
+
+        price.setText(summary);
     }
     public void next(View view){
         Button next= (Button)findViewById(R.id.next);
